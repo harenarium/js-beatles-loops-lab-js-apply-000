8cf6baa214015260cafcfa224b798bb70d4262a5
harenarium <carolinel2610@gmail.com>
2017-12-09 18:51:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments){
+  const array = {}
+  for(i=0; i<musicians.length; i++){
+    const string = musicians[i] plays instruments[i]
+    array.push(string)
+  }
+}
